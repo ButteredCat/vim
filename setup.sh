f7376956579708f34d0d1abc51235545a52d743a
ButteredCat <yunchen.xiao@gmail.com>
2015-02-20 09:44:37 -0500

    won't setup if config files already exist

--- setup.sh
+++ setup.sh
@@ -2,5 +2,17 @@
 
 # setup vim config file for current user
 
-cp .vimrc ${HOME}/ 
-cp -r .vimrc ${HOME}/
+VIMRC_TARGT=${HOME}/.vimrc
+VIM_TARGT=${HOME}/.vim
+
+if test  -e ${VIMRC_TARGT}; then
+	echo "${VIMRC_TARGT} already exists! Please remove first."
+else
+	cp .vimrc ${HOME}/ 
+fi
+
+if test -e ${VIM_TARGT}; then
+	echo "${VIM_TARGT} already exists! Please remove first."
+else
+	cp -r .vimrc ${HOME}/
+fi
